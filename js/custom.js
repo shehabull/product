d49c92dcbc4b086e787509876cd1ead0748d3beb
shehabull <shehabull@gmail.com>
2015-08-05 02:07:39 +0600

    Adding a new file

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+      
+  var totalPrice = 0;
+
+  function savedata () {
+
+    var name = document.querySelector('#name').value;
+    var price = document.querySelector('#price').value;
+    var quentity = document.querySelector('#quentity').value;
+    totalPrice += quentity*price;
+
+
+    var table = document.getElementById("productTable");
+
+    // add raw:
+    var row = table.insertRow(1);
+
+    // Insert new cells:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    // Add cells value:
+    cell1.innerHTML = name;
+    cell2.innerHTML = price;
+    cell3.innerHTML = quentity;
+
+
+    document.getElementById('total').innerHTML  = totalPrice;
+
+  }
