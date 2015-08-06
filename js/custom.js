75566258c312e46d25eb8d4e03514c090b470db2
shehabull <shehabull@gmail.com>
2015-08-06 23:36:56 +0600

    for assignment two print function

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,30 @@
-      
-  var totalPrice = 0;
-
-  function savedata () {
-
-    var name = document.querySelector('#name').value;
-    var price = document.querySelector('#price').value;
-    var quentity = document.querySelector('#quentity').value;
-    totalPrice += quentity*price;
+    var totalPrice = 0;
+    function savedata () {
+        var name = document.querySelector('#name').value;
+        var price = document.querySelector('#price').value;
+        var quentity = document.querySelector('#quentity').value;
+        totalPrice += quentity*price;
 
 
-    var table = document.getElementById("productTable");
+        var table = document.getElementById("productTable");
 
-    // add raw:
-    var row = table.insertRow(1);
+        // add raw:
+        var row = table.insertRow(1);
 
-    // Insert new cells:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
+        // Insert new cells:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
 
-    // Add cells value:
-    cell1.innerHTML = name;
-    cell2.innerHTML = price;
-    cell3.innerHTML = quentity;
+        // Add cells value:
+        cell1.innerHTML = name;
+        cell2.innerHTML = price;
+        cell3.innerHTML = quentity;
+        document.getElementById('total').innerHTML  = totalPrice;
+    }
 
 
-    document.getElementById('total').innerHTML  = totalPrice;
-
-  }
+    function showBrochure(){
+        document.querySelector('#product_list').style.display='none';
+        document.querySelector('#product_brochure').style.display='block';
+    }
